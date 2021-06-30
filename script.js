cb3441a4e54160ff906d95c4ded4490ed022c8d5
Jayasoorya <jayaspassion@gmail.com>
2021-07-01 00:01:33 +0530

    Almost Design

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
 // getting data
 var user_data_link = "https://raw.githubusercontent.com/Rajavasanthan/jsondata/master/pagenation.json";
+var rows = 8;
+var pageNumber = 1;
+
+
+function displayUsers(arr, page, currentRows){
+    let currentUsers = arr.slice(((page-1)*currentRows), page*currentRows);
+    const users = document.querySelector(".users");
+    currentUsers.forEach(user => {
+        let userCard = document.createElement('div');
+        userCard.className = 'user-card';
+        userCard.innerHTML=`
+        <div class="user-image"><img src="https://picsum.photos/50"></div>
+        
+        <div>
+        <p>${user.name}</h3>
+        <p class="user-email">${user.email}</p>
+        </div>
+        `;
+        users.append(userCard);
+    });
+}
 
 async function getUsers(){
-    const response = await fetch(user_data_link).then(console.log("Fetched"));
+    const response = await fetch(user_data_link);
     const data = await response.json();
+    displayUsers(data, pageNumber, rows);
     console.log(data);
-    return data;
+    
 }
-getUsers();
+
 
 
 
@@ -17,38 +39,23 @@ const wrapper = document.createElement('div');
 wrapper.className = 'wrapper';
 
 
-//main 3 divs for body
-// const uiDiv = document.createElement('div');
-// uiDiv.className = 'ui-div';
-// const users = document.createElement('div');
-// users.className = 'users';
-// const paginationDiv = document.createElement('div');
-// paginationDiv.className = 'pagination-div';
+
 wrapper.innerHTML = `
 <header class="ui-div">
     <img src="https://picsum.photos/700/400"></img>
 </header>
-<article class="users">Article</article>
+<article class="users"></article>
 <footer class="pagination-div">
     <div class="pagination"></div>
 
 </footer>
 `
+
+
 //appending elements
 document.body.appendChild(wrapper);
-// wrapper.append(uiDiv, users, paginationDiv);
 
 
 
 
-// async function myApi(){
-//    const userDataList = await fetch("https://raw.githubusercontent.com/Rajavasanthan/jsondata/master/pagenation.json")
-//    var data = await userDataList.json();
-//    return data;
-// }
-
-// const answer = myApi();
-
-// console.log(answer);
-
-
+getUsers();
